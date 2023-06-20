335a193bb8d8ae8918670094743ae2e2071b9847
Jordan Marte <jordan.marte@bionuclear.com.do>
2023-06-19 22:39:02 -0400

    Ejecicios M03 concluidos

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -51,6 +51,8 @@ function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
+
+   return lado * 4;
 }
 
 function areaDelTriangulo(base, altura) {
